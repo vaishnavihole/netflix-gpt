6aa9401ad30ceeef4840b3bfef351e03ed918e2e
vaishnavihole <vaishnavihole1@gmail.com>
2023-09-05 22:27:00 +0530

    feat:body image and header are added

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import Header from './Header.js';
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+        alt=""/>
+      </div>
+    
+    </div>
+  )
+}
+
+export default Login;
