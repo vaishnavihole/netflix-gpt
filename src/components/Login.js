e8dd04bb6feaa11ad3d2e640dd333026551fee19
vaishnavihole <vaishnavihole1@gmail.com>
2023-09-06 07:44:35 +0530

    feat: design login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,11 +4,28 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_small.jpg"
-        alt=""/>
+        alt="logo"/>
+
+
       </div>
-    
+       <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+
+        <h1 className="font-bold text-3xl py-4">
+          Sign In</h1>
+
+        <input type="text" placeholder="Email Address"
+         className='p-3 my-4 w-full bg-gray-700'/>
+
+        <input type="password" placeholder="Password" 
+        className='p-3 my-4 w-full bg-gray-700'/>
+
+        <button className="p-3 my-6 bg-red-700 w-full rounded-lg">
+          Sign In</button>
+
+          <p className="py-6">New To Netflix? Sign Up Now</p>
+       </form>
     </div>
   )
 }
